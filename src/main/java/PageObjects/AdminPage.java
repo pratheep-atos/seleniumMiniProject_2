e1906d08b5e50aa6bb7a1fdf41988e1aa0197d3a
1623033@saec.ac.in <google.com>
2024-05-24 23:21:53 +0530

    changes

--- src/main/java/PageObjects/AdminPage.java
+++ src/main/java/PageObjects/AdminPage.java
@@ -13,18 +13,82 @@ public class AdminPage {
 	public AdminPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
-	}
+	}	
 	
 	@FindBy(css = ".oxd-main-menu-item")
 	private WebElement AdminSection;
 	
 	@FindBy(css = ".oxd-table-row .oxd-table-cell:nth-child(2)")
 	private List<WebElement> UserName;
-
-
+	
+	
+	
+	@FindBy(xpath = "//div[@class='orangehrm-header-container'] //button[contains(@class,'oxd-button')]")
+	private WebElement AddAccount;
+	
+	@FindBy(xpath = "(//div[@class='oxd-select-text-input'])[1]")
+	private WebElement UserRole;
+	
+	
+	@FindBy(xpath = "//div[@class='oxd-select-option'] //span[text()='Admin']")
+	private WebElement AdminDropDown;
+	
+	@FindBy(xpath = "//input[contains(@placeholder,'Type for hints')]")
+	private WebElement EmployeeName;
+	
+	@FindBy(css = ".oxd-autocomplete-option")
+	private List<WebElement> employeeOptions;
+	
+	@FindBy(xpath = "(//div[@class='oxd-select-text-input'])[2]")
+	private WebElement Status;
+	
+	@FindBy(xpath = "//div[@class='oxd-select-option'] //span[text()='Enabled']")
+	private WebElement StatusDropDown;
+	
+	@FindBy(css = ".oxd-input:nth-child(1)")
+	private WebElement username;
+	
+	@FindBy(xpath = "(//input[@type='password'])[1]")
+	private WebElement password;
+	
+	@FindBy(xpath = "(//input[@type='password'])[2]")
+	private WebElement ConfirmPassword;
+	
+	@FindBy(xpath = "//button[@type='submit']")
+	private WebElement Save;	
+	
 	public List<WebElement> adminPageUsernames() {
 		 AdminSection.click();
 		 return UserName;
+	}
+	
+	public void addAccount() {
+		AdminSection.click();
+		AddAccount.click();	
+	}
+	
+	public void SelectUser() {
+		UserRole.click();
+		AdminDropDown.click();
+	}
+	public void SelectEmployee() {
+		EmployeeName.sendKeys("peter");
+	}	
+	
+	public List<WebElement> employeeNames(){
+		return employeeOptions;
+	}
+	
+	public void SelectStatus() {
+		Status.click();
+		StatusDropDown.click();
+	}
+	
+	public void usernamePassword(String adminUsername ,String adminPassword) {
+		username.sendKeys(adminUsername);
+		password.sendKeys(adminPassword);
+		ConfirmPassword.sendKeys(adminPassword);
+		Save.click();
 		
 	}
 }
---
+++ src/test/java/com/SeleniumFramework/TestCases/TC_02_AddAccount.java
@@ -0,0 +1,43 @@
+package com.SeleniumFramework.TestCases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import BaseTest.BaseTest;
+import PageObjects.AdminPage;
+import PageObjects.LoginPage;
+import Utils.ConstantsHelper;
+
+public class TC_02_AddAccount extends BaseTest{
+	
+	@Test
+	public void AddAccountValidation(){
+		LoginPage loginPage = new LoginPage(driver);
+    	AdminPage adminPage= loginPage.Login(ConstantsHelper.username, ConstantsHelper.password); 
+    	adminPage.addAccount();
+    	adminPage.SelectUser();
+    	adminPage.SelectEmployee();
+    	WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(6));
+    	wait.until(ExpectedConditions.visibilityOfAllElements(adminPage.employeeNames()));
+    	for(WebElement empy:adminPage.employeeNames()) {
+    		String empynName =empy.getText();
+    		if(empynName.contains("Peter Mac")) {
+    			empy.click();
+    		}	
+    	}
+    	adminPage.SelectStatus();
+    	String username ="pravinueq";
+    	String password ="pravinueq12";
+    	adminPage.usernamePassword(username,password);
+		
+		
+	}
+	
+	
+	
+
+}
